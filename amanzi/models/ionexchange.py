26900f49f9e5541bf5096173a98911207722439f
Christiaan <christiaan@Christiaans-MacBook-Pro.local>
2023-05-16 14:49:06 +0200

    IEX poc finished

--- amanzi/models/ionexchange.py
+++ amanzi/models/ionexchange.py
@@ -4,19 +4,33 @@ from .submodels.balance import Balance
 class Ionexchange(Model, Balance):
     def __init__(self, config, pp):
         super().__init__(config, pp)
-        # self.config = config
         self.configuration = config.get('configuration', {})
-        # self.ion_exchange = self.configuration.get('ion_exchange', {'Na': 0.9, 'Cl': 0.9, 'Mg': 0.5, 'Ca': 0.5})
-        self.iex_coefficients = self.configuration.get('iex_coefficients', {'Cl': 0.95, 'SO4': 0.9, 'NO3': 0.92, 'NO2':0.9})
 
-        self.resin_capacity = self.configuration.get('resin_capacity', 10) #10kg default capacity
+        ## Future database parameters
+        self.resin = self.configuration.get('resin', 'Purolite-A860S')
+        self.iex_coefficients = self.configuration.get('iex_coefficients', [('Toc','Cl', 1, 0.95)]) #[water_ion, resin_ion, molratio, efficieny]
+        self.resin_capacity = self.configuration.get('resin_capacity', 5000) #5000kg Cl- capacity
+        ## Future database parameters
+
+        
         self.resin_load = self.configuration.get('resin_load', 0)
-
         self.regenerations = 0
         
     def run_model(self, type, total_inflow, solution):
-        return solution
+        solution_change = {}
+        for (water_ion, resin_ion, molratio, eff) in self.iex_coefficients:
+            # Ion change in water
+            ion_removed = solution.total(water_ion) * molratio * eff #in abs(mmol)
+            solution_change[water_ion] = -ion_removed
+            solution_change[resin_ion] = ion_removed
+
+            # Ion change in resin (for regeneration tracking)
+            mw = 180.16 #MW of Toc (assuming glucose)
+            self.resin_load += ion_removed * total_inflow * mw * 1e-6 #in kg
+        
+        self.regenerations = self.resin_load // self.resin_capacity 
+    
         effluent = solution.copy()
-        for el, ret in self.ion_exchange.items():
-            pass
-            effluent.change({el: solution.total(el, 'mmol') * ret}, 'mmol')
+        effluent.change(solution_change)
+            
+        return effluent
