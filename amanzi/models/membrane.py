1bc306acf079af826013f2438b5c13ef749256a6
Christiaan <christiaan@Christiaans-MacBook-Pro.local>
2023-08-22 13:39:27 +0200

    membrane db integrated

--- amanzi/models/membrane.py
+++ amanzi/models/membrane.py
@@ -30,6 +30,21 @@ MEMBRANES_DATABASE =  {
                 "recovery": 10, # %
                 "temperature": 25, # C
             }
+        },
+        "ESPA2": {
+            "nominal_flow": 27.3, #m3/day
+            "salt_rejection": 99.7, # %
+            "retention": {"Na": 0.997, "Cl": 0.997},
+            "feed_flow_max": 17.0, #m3/h
+            "dP_max_element": 1.0, # bar
+            "flux_avg": 15., # L/m2h
+            "A_e": 40.9, # m2
+            "test_conditions": {
+                "C_feed": 32000, # mg/L NaCl
+                "P_feed": 55, # bar
+                "recovery": 10, # %
+                "temperature": 25, # C
+            }
         }
     }
 
