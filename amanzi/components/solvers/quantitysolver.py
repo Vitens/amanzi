c4e2f16f5567e1a7a2254e9743253e112d9fc907
Abel Heinsbroek <mail@abelheinsbroek.nl>
2025-05-22 13:54:07 +0200

    add number of elements to hydraulic line

--- amanzi/components/solvers/quantitysolver.py
+++ amanzi/components/solvers/quantitysolver.py
@@ -83,8 +83,6 @@ class QuantitySolver(Solver):
         generate a summary of the solver
         """
 
-
-
         production = sum([m.quantity.outflow.get('product', 0) for _,m in self.scenario.models.items() if m.upstream_connections == {}])
 
         distribution = sum([m.quantity.inflow.get('product', 0) for _,m in self.scenario.models.items() if m.downstream_connections == {}])
