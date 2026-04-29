b51bbabdb121c73090ec7b10470c8da4aaae730b
ChristiaanSl <christiaan@Christiaans-MacBook-Pro.local>
2026-04-29 13:44:40 +0200

    Citation-info in About

--- ui/src/components/Dialogs/About.vue
+++ ui/src/components/Dialogs/About.vue
@@ -16,6 +16,28 @@
         <pre>{{ acknowledgements }}</pre>
         <template #title><i class="fa fa-users"></i> {{ $t('ui.dialogs.about.acknowledgements') }}</template>
       </el-collapse-item>
+      <el-collapse-item name="citation">
+        <div class="citation-section">
+          <p class="citation-hint">
+            {{ $t('ui.dialogs.about.citation.description') }}
+          </p>
+          <div class="citation-block">
+            <h3>{{ $t('ui.dialogs.about.citation.plain_text') }}</h3>
+            <pre>{{ plainTextCitation }}</pre>
+            <el-button size="small" @click="copyCitation(plainTextCitation)">
+              {{ $t('ui.dialogs.about.citation.copy_plain_text') }}
+            </el-button>
+          </div>
+          <div class="citation-block">
+            <h3>{{ $t('ui.dialogs.about.citation.bibtex') }}</h3>
+            <pre>{{ bibtexCitation }}</pre>
+            <el-button size="small" @click="copyCitation(bibtexCitation)">
+              {{ $t('ui.dialogs.about.citation.copy_bibtex') }}
+            </el-button>
+          </div>
+        </div>
+        <template #title><i class="fa fa-quote-right"></i> {{ $t('ui.dialogs.about.citation.title') }}</template>
+      </el-collapse-item>
       <el-collapse-item :title="$t('ui.dialogs.about.license')" name="license">
         <pre>{{ license }}</pre>
         <template #title><i class="fa fa-balance-scale"></i> {{ $t('ui.dialogs.about.license') }}</template>
@@ -28,6 +50,32 @@ import changelog from '@@/changelog.md?raw'
 import acknowledgements from '@@/ACKNOWLEDGEMENTS?raw'
 import license from '@@/LICENSE?raw'
 export default {
+  computed: {
+    plainTextCitation() {
+      return `Vitens. Amanzi (Version ${this.$version}) [Computer software]. ${new Date().getFullYear()}. https://github.com/Vitens/Amanzi`
+    },
+    bibtexCitation() {
+      return `@software{amanzi_${new Date().getFullYear()},\n  author = {Vitens},\n  title = {Amanzi},\n  version = {${this.$version}},\n  year = {${new Date().getFullYear()}},\n  url = {https://github.com/Vitens/Amanzi}\n}`
+    }
+  },
+  methods: {
+    copyCitation(value) {
+      if (navigator?.clipboard?.writeText) {
+        navigator.clipboard.writeText(value)
+        return
+      }
+
+      const textarea = document.createElement('textarea')
+      textarea.value = value
+      textarea.style.position = 'fixed'
+      textarea.style.opacity = '0'
+      document.body.appendChild(textarea)
+      textarea.focus()
+      textarea.select()
+      document.execCommand('copy')
+      document.body.removeChild(textarea)
+    }
+  },
   data() {
     return {
       changelog,
@@ -85,5 +133,22 @@ export default {
   font-size: 0.9em;
   color: #666;
   text-align: justify;
+}
+.citation-section {
+  margin-top: 4px;
+  text-align: left;
+}
+.citation-hint {
+  margin-bottom: 12px;
+}
+.citation-block {
+  margin-bottom: 12px;
+}
+.citation-block h3 {
+  margin-bottom: 6px;
+  font-size: 1em;
+}
+.citation-block pre {
+  margin: 0 0 8px;
 }
 </style>
